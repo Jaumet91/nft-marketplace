d64ebc5515c8a266c5ff92e90b71b598cc1ad55a
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-27 11:59:45 +0200

    Navbar.tsx: logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,31 @@
+import { useState, useEffect, useContext } from 'react'
+import { useRouter } from 'next/router'
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import images from '../assets'
+
 type Props = {}
 
 export const Navbar = (props: Props) => {
-  return <div>navbar</div>
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b bg-white dark:bg-nft-dark border-nft-gray-1 dark:border-nft-black-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+          </div>
+        </Link>
+      </div>
+    </nav>
+  )
 }
