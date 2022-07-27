f204eda99e6780a093c9448d0257160b8224d79b
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-27 11:32:49 +0200

    Base routing done

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+type Props = {}
+
+export const Footer = (props: Props) => {
+  return <div>footer</div>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import Script from 'next/script'
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components'
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script
+      src="https://kit.fontawesome.com/ab656263f4.js"
+      crossOrigin="anonymous"
+    />
+  </ThemeProvider>
+)
 
 export default MyApp
