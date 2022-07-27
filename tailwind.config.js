d125aa074a27af9439195224839b620499b54457
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-27 10:53:16 +0200

    edited README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: 'class',
   theme: {
@@ -20,12 +20,12 @@ module.exports = {
         'file-active': '#2196f3',
         'file-accept': '#00e676',
         'file-reject': '#ff1744',
-        'overlay-black': 'rgba(0, 0, 0, 0.8)',
+        'overlay-black': 'rgba(0, 0, 0, 0.8)'
       },
       width: {
         215: '215px',
         357: '357px',
-        557: '557px',
+        557: '557px'
       },
       minWidth: {
         155: '155px',
@@ -33,29 +33,29 @@ module.exports = {
         215: '215px',
         240: '240px',
         256: '256px',
-        327: '327px',
+        327: '327px'
       },
       height: {
         300: '300px',
-        557: '557px',
+        557: '557px'
       },
       inset: {
         45: '45%',
-        65: '65px',
+        65: '65px'
       },
       spacing: {
-        65: '65px',
+        65: '65px'
       },
       flex: {
-        2: '2 2 0%',
+        2: '2 2 0%'
       },
       lineHeight: {
-        70: '70px',
+        70: '70px'
       },
       zIndex: {
         '-5': '-5',
-        0: '0',
-      },
+        0: '0'
+      }
     },
     screens: {
       lg: { max: '1800px' },
@@ -63,14 +63,14 @@ module.exports = {
       sm: { max: '600px' },
       xs: { max: '400px' },
       minmd: '1700px',
-      minlg: '2100px',
+      minlg: '2100px'
     },
     fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-    },
+      poppins: ['Poppins', 'sans-serif']
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
