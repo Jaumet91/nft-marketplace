a182f91050eeaac42d13f248133b6fbf0caa5af9
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-27 12:32:46 +0200

    Navbar.tsx: responsiveness added to logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,10 +12,27 @@ export const Navbar = (props: Props) => {
   const { theme, setTheme } = useTheme()
 
   return (
-    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b bg-white dark:bg-nft-dark border-nft-gray-1 dark:border-nft-black-1">
+    <nav className="flexBetween fixed z-10 w-full flex-row border-b border-nft-gray-1 bg-white p-4 dark:border-nft-black-1 dark:bg-nft-dark">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
-          <div>
+          <div
+            className="flexCenter cursor-pointer md:hidden"
+            onClick={() => {}}
+          >
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="ml-1 text-lg font-semibold text-nft-black-1 dark:text-white">
+              CryptoHue
+            </p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
             <Image
               src={images.logo02}
               objectFit="contain"
