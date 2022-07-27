0350151f44c531caa2094402ffa1c0e88063cbff
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-27 14:09:02 +0200

    Button.tsx: btnName prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,15 @@
-type Props = {}
+import PropTypes from 'prop-types'
+type Props = { classStyles: string; btnName: string }
 
-export const Button = (props: Props) => (
-  <button type="button" className={'nft-gradient '}>
-    Button
+export const Button = ({ classStyles, btnName }: Props) => (
+  <button
+    type="button"
+    className={`nft-gradient py-2 px-6 font-poppins text-sm font-semibold text-white minlg:px-8 minlg:text-lg ${classStyles}`}>
+    {btnName}
   </button>
 )
+
+Button.propTypes = {
+  classStyles: PropTypes.string.isRequired,
+  btnName: PropTypes.string.isRequired
+}
--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -5,5 +5,9 @@ type Props = {}
 export const ButtonGroup = (props: Props) => {
   const hasConnected = false
 
-  return hasConnected ? <Button /> : <Button />
+  return hasConnected ? (
+    <Button btnName="Create" classStyles="mx-2 rounded-xl" />
+  ) : (
+    <Button btnName="Connect" classStyles="mx-2 rounded-xl" />
+  )
 }
