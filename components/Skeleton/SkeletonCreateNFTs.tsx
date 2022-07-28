ea1994d5f561ca2ddc4050dc2377ecbd39b0fbaa
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-28 19:07:43 +0200

    SkeletonCreateNFTs.tsx: Created

---
+++ components/Skeleton/SkeletonCreateNFTs.tsx
@@ -0,0 +1,3 @@
+export const SkeletonCreateNFTs = () => {
+  return <div>SkeletonCreateNFTs</div>
+}
--- pages/create-nft.tsx
+++ pages/create-nft.tsx
@@ -1,9 +1,23 @@
-import { useState, useMemo, useCallback, useContext } from 'react'
+import { useState, useMemo, useCallback, useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useDropzone } from 'react-dropzone'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+
+import { Button } from '../components'
+import images from '../assets'
+import { SkeletonCreateNFTs } from '../components/Skeleton'
 
 const CreateNFT = () => {
-  return <div>create-nft</div>
+  const { theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const [fileUrl, setFileUrl] = useState(null)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return <SkeletonCreateNFTs />
 }
 
 export default CreateNFT
