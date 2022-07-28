cc6265c3d69e5a95d9fd0fcda3f94e237aaca754
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-28 12:16:59 +0200

    Banner.tsx: Styles added

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,15 @@
-type Props = {}
+type Props = { name: string; childStyles: string; parentStyles: string }
 
-export const Banner = (props: Props) => {
-  return <div>Banner</div>
+export const Banner = ({ name, childStyles, parentStyles }: Props) => {
+  return (
+    <div
+      className={`nft-gradient relative z-0 flex items-center overflow-hidden ${parentStyles} `}>
+      <p
+        className={`font-poppins text-5xl font-bold leading-70 ${childStyles}`}>
+        {name}
+      </p>
+      <div className="white-bg absolute -top-9 -left-16 -z-5 h-48 w-48 rounded-full sm:h-32 sm:w-32" />
+      <div className="white-bg absolute -bottom-24 -right-14 -z-5 h-72 w-72 rounded-full sm:h-56 sm:w-56" />
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import { Banner } from '../components'
 const Home: NextPage = () => (
   <div className="flex justify-center p-12 sm:px-4">
     <div className="w-full minmd:w-4/5">
-      <Banner />
+      <Banner
+        name="Discover, collect, and sell extraordinary NFTs"
+        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+      />
     </div>
   </div>
 )
