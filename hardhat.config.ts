8f8fe422b2c4786d8a61a9a9ccefaaa49df64a4b
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-29 07:22:36 +0200

    NFTMarketplace.sol: created

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomiclabs/hardhat-waffle'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9'
+  solidity: '0.8.15'
 }
 
 export default config
