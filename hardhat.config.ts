783fd48b97891b9bd4f90f99d35cd87e381c6163
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-29 09:59:26 +0200

    hardhat.config.ts: implemented deployment functionality

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,15 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomiclabs/hardhat-waffle'
+import * as fs from 'fs'
+
+const privateKey = fs.readFileSync('.secret').toString().trim()
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
   solidity: '0.8.15'
 }
 
