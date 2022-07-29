a689c5f53f049e4528a594599502de4b04e60da7
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-29 08:17:25 +0200

    NFTMarketplace.sol: resell token function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -91,4 +91,24 @@ contract NFTMarketplace is ERC721URIStorage {
             false
         );
     }
+
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+        require(
+            idToMarketItem[tokenId].owner == msg.sender,
+            "Only item owner can perform this operation"
+        );
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId);
+    }
 }
