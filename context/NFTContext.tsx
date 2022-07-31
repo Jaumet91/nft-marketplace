bc900f79f7aef7358ae6c5bf63a711b425e8c6c4
Jaime Cortes <hello@jaimecortesdev.com>
2022-07-31 20:22:33 +0200

    NFTContext.tsx: uploadToIPFS function implemented

--- context/NFTContext.tsx
+++ context/NFTContext.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, createContext } from 'react'
 import Web3Modal from 'web3modal'
 import { ethers } from 'ethers'
 import axios from 'axios'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
 
 import { MarketAddress, MarketAddressABI } from './constants'
 
@@ -9,12 +10,15 @@ interface NFTContextProps {
   nftCurrency: string
   connectWallet: () => Promise<void>
   currentAccount: string
+  uploadToIPFS: (file: File) => Promise<string | undefined>
 }
 
 interface props {
   // eslint-disable-next-line no-undef
   children: JSX.Element | JSX.Element[]
 }
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
 export const NFTContext = createContext<NFTContextProps>({} as NFTContextProps)
 
@@ -46,8 +50,20 @@ export const NFTProvider = ({ children }: props) => {
     window.location.reload()
   }
 
+  const uploadToIPFS = async (file: File) => {
+    try {
+      const added = await client.add({ content: file })
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+
+      return url
+    } catch (error) {
+      console.error('Error uploading file to IPFS.')
+    }
+  }
+
   return (
-    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
+    <NFTContext.Provider
+      value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}>
       {children}
     </NFTContext.Provider>
   )
